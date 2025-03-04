fe7d81d2266b137635ee462badc060da739d302f
tarryn-masunda <tarrynmasunda@gmail.com>
2025-03-04 11:39:46 +0200

    correct products code

--- model/productModel.js
+++ model/productModel.js
@@ -1,9 +1,13 @@
 import { pool } from '../config/config.js';
 
 const getProducts = async () => {
-    const [products] = await pool.query("SELECT * FROM products");
+    const [products] = await pool.query(`SELECT *
+FROM product_images
+INNER JOIN products
+ON product_images.product_id = products.product_id;`);
     return products;
 };
+
 
 const singleProducts = async (id) => {
     const [product] = await pool.query("SELECT * FROM products WHERE product_id = ?", [id]);
